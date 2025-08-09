8e120e6119ecef538f3f8488468c101c6641ecbe
anju s <anjusrinivasan007@gmail.com>
2025-08-09 18:09:15 +0530

    Added scroll animation basic and clients section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import HeroSection from './components/HeroSection';
+import ClientsSection from './components/ClientsSection';
 import AboutSection from './components/AboutSection';
 import PortfolioSection from './components/PortfolioSection';
 import ContactSection from './components/ContactSection';
@@ -13,25 +14,28 @@ import Footer from './components/Footer';
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  // useEffect(() => {
-  //   gsap.utils.toArray('.section').forEach((section) => {
-  //     gsap.from(section, {
-  //       opacity: 0,
-  //       y: 80,
-  //       duration: 1,
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         start: 'top 80%',
-  //         toggleActions: 'play none none reverse'
-  //       }
-  //     });
-  //   });
-  // }, []);
+  console.log("Sections found:", gsap.utils.toArray(".section"));
+
+  useEffect(() => {
+    gsap.utils.toArray('.section').forEach((section) => {
+      gsap.from(section, {
+        opacity: 0,
+        y: 80,
+        duration: 1,
+        scrollTrigger: {
+          trigger: section,
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      });
+    });
+  }, []);
 
   return (
     // <div className="bg-white text-black ">
     <div className="bg-white text-black font-anton">
       <HeroSection />
+      <ClientsSection />
       <AboutSection />
       <PortfolioSection />
       <ContactSection />
