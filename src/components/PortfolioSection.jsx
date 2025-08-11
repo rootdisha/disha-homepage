87b6a4b8c929e7a3b9d1914164247d3a9a997794
anju s <anjusrinivasan007@gmail.com>
2025-08-11 13:35:47 +0530

    hashtag

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -2,7 +2,7 @@
 
 export default function PortfolioSection() {
     return (
-      <section className="section min-h-screen flex flex-col justify-center items-center px-6">
+      <section className="section min-h-screen flex flex-col justify-center items-center px-6" id="work">
         <h2 className="text-8xl  mb-4 text-primary uppercase">Our Work</h2>
         <div className="grid md:grid-cols-3 gap-6 mt-8">
           <div className="bg-black text-secondary p-4 rounded-lg">Project 1</div>
