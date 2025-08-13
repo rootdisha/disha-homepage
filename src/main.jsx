fa02e617b0087d67cad630a6eaee3a466e08d4e1
anju s <anjusrinivasan007@gmail.com>
2025-08-13 22:55:54 +0530

    removed old react components

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import App_claude from './App_claude.jsx'
+import App from './App_claude.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <App_claude />
+    <App />
   </StrictMode>
 )
